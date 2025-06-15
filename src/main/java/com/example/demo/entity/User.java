e40fd4810961d35667d82b5541b2ba89bf70d487
foura1201 <foura3207@gmail.com>
2025-06-15 12:45:45 +0900

    사용자 역할 추가

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,7 +1,10 @@
 package com.example.demo.entity;
 
+import com.example.demo.common.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -28,4 +31,8 @@ public class User {
 
   @Column(name = "nickname")
   private String nickname;
+
+  @Column(name = "role", nullable = false)
+  @Enumerated(value = EnumType.STRING)
+  private Role role;
 }
