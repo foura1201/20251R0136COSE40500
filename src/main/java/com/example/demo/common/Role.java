6f62f982b404a1bb4002323037a611f75f184400
foura1201 <foura3207@gmail.com>
2025-06-15 12:45:09 +0900

    사용자 역할 정의

---
+++ src/main/java/com/example/demo/common/Role.java
@@ -0,0 +1,17 @@
+package com.example.demo.common;
+
+import lombok.Getter;
+
+@Getter
+public enum Role {
+  USER("user", "일반 사용자"),
+  ADMIN("admin", "관리자");
+
+  private final String name;
+  private final String description;
+
+  Role(String name, String description) {
+    this.name = name;
+    this.description = description;
+  }
+}
