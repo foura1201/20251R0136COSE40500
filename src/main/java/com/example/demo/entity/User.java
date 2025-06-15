63b07009448aa29df793b69c04f0889ffa69136c
foura1201 <foura3207@gmail.com>
2025-06-15 12:50:03 +0900

    user의 email에 제한 조건 생성

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -23,7 +23,7 @@ public class User {
   @Column(name = "id")
   private Long id;
 
-  @Column(name = "email")
+  @Column(name = "email", unique = true, nullable = false)
   private String email;
 
   @Column(name = "name")
